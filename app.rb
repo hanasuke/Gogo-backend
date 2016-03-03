919cd3d0a18feb912fabe9b438299bab1cc8fe06
naosuke <naosuke@live.jp>
2016-03-03 22:56:59 +0900

    add sinatra/reloader

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'json'
 
 post '/' do
   content_type :json
-  data = { message: "recieve post data", status: true}
+  data = { message: "recieve post data", status: "200"}
   data.to_json
 end
